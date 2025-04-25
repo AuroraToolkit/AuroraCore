a10d6eea61d1354586b68151c6df53c27397807e
Dan Murrell <danmurrelljr@gmail.com>
2025-04-24 19:33:21 -0500

    Separate LLMDomainRouterProtocol into its own file
    - Move LLMDomainRouter into Routers folder

--- Sources/AuroraLLM/Routers/LLMDomainRouter.swift
+++ Sources/AuroraLLM/Routers/LLMDomainRouter.swift
@@ -1,53 +1,12 @@
 //
 //  LLMDomainRouter.swift
-//  AuroraCore
+//  AuroraToolkit
 //
-//  Created by Dan Murrell Jr on 12/11/24.
+//  Created by Dan Murrell Jr on 4/24/25.
 //
 
-
-import Foundation
 import os.log
 import AuroraCore
-
-/// Protocol defining the behavior of a domain router in the LLMManager system.
-public protocol LLMDomainRouterProtocol {
-
-    /// The name of the domain router, used for logging and identification purposes.
-    var name: String { get }
-
-    /// A list of domains that the router supports.
-    var supportedDomains: [String] { get }
-
-    /**
-        Determines the domain for a given request using the associated LLM service.
-
-        - Parameters:
-            - request: The `LLMRequest` containing the prompt or context for domain determination.
-
-        - Returns: A string representing the determined domain, or `nil` if not posslbe.
-     */
-    func determineDomain(for request: LLMRequest) async throws -> String?
-}
-
-
-/**
-    Protocol defining the behavior of a domain router that can provide confidence scores for domain determination.
-
-    - Note: This protocol extends `LLMDomainRouterProtocol` to include a method for determining the domain with a confidence score.
- */
-public protocol ConfidentDomainRouter: LLMDomainRouterProtocol {
-    /**
-        Determines the domain for a given request using the associated LLM service.
-
-        - Parameters:
-            - request: The `LLMRequest` containing the prompt or context for domain determination.
-
-        - Returns: A string representing the determined domain, and double representing confidence, or `nil` if not possible.
-     */
-    func determineDomainWithConfidence(for request: LLMRequest) async throws -> (String, Double)?
-}
-
 
 /**
     The `LLMDomainRouter` class is responsible for determining the domain of a request using an LLM service.
